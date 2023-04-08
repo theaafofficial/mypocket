8cba00df0bf3aa6320ef19cc345559fe09699d6c
Ahmad A <ahmad@dataprism.me>
2023-04-08 23:20:00 +0500

    added session & layout providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import { type AppType } from "next/app";
-
+import { RecoilRoot } from "recoil";
 import { api } from "~/utils/api";
+import { SessionProvider } from "next-auth/react";
+import Layout from "~/components/Layout";
+import type { NextComponentType } from "next";
+import type { Session } from "next-auth";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+export interface AppProps {
+  Component: NextComponentType;
+  session?: Session;
+  pageProps?: any;
+}
+
+const MyApp: AppType = ({ Component, session, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </SessionProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
