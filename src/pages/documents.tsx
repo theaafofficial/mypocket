45aca5479d34bf451f36811c1bb14ed4327522f6
Ahmad A <ahmad@dataprism.me>
2023-05-05 11:08:32 +0500

    Changed title to Documents

--- src/pages/documents.tsx
+++ src/pages/documents.tsx
@@ -9,7 +9,7 @@ const documents: NextPage = () => {
 
   return (
     <>
-      <File result={document} MediaType="Document" title="All documents" uri="/documents" whole_page={false} />
+      <File result={document} MediaType="Document" title="Documents" uri="/documents" whole_page={false} />
     </>
   );
 };
