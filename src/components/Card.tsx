c2f2834f820b78f0258b52f38e88afea5faf1fbf
Ahmad A <ahmad@dataprism.me>
2023-05-05 10:49:27 +0500

    added starred icon and few fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,9 @@ import { truncate } from "~/utils/helper";
 import FileDeteteModal from "./FileDelete";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useMediaQuery } from "usehooks-ts";
+import { api } from "~/utils/api";
+import { MdStar } from "react-icons/md";
 
 export interface CardProps {
   title: string | undefined;
@@ -13,7 +16,8 @@ export interface CardProps {
   extension?: string | undefined;
   public_id?: string | undefined;
   resource_type?: string | undefined;
-  refetch?: () => void;
+  isStarred?: boolean;
+  refetch: () => void;
   isImage?: boolean;
   onSelect?: () => void;
   selected?: boolean;
@@ -26,26 +30,32 @@ const Card: React.FC<CardProps> = ({
   extension,
   public_id,
   resource_type,
+  isStarred,
   refetch,
   isImage,
   onSelect,
   selected,
 }) => {
+  const isMobile = useMediaQuery("(max-width: 640px)");
   const router = useRouter();
+  const starred = api.router.setStarredProperty.useMutation({
+    onSuccess: () => {
+      void refetch();
+    },
+  });
   return (
     <div
-      className={`group relative block h-36 w-36 sm:h-56 sm:w-56 cursor-pointer`}
+      className={`group relative block h-36 w-36 cursor-pointer sm:h-56 sm:w-56`}
     >
       <span
         className={`absolute inset-0 border-2 border-dashed ${
-          selected ? "border-gray-200" : "border-black"
+          !selected ? "border-gray-300" : "border-black"
         }`}
       ></span>
       <div
         className={`relative flex h-full transform items-end border-2 ${
-          selected ? "border-gray-200" : "border-black"
+          !selected ? "border-gray-300" : "border-black"
         } bg-white transition-all ease-out  group-hover:-translate-y-2`}
-        onClick={onSelect}
       >
         <div className="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
           {isImage ? (
@@ -71,20 +81,29 @@ const Card: React.FC<CardProps> = ({
         </div>
 
         <div className="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
-          <h3
-            className={`text-md mt-4 mb-2 select-none font-medium sm:text-2xl `}
-            title={title}
-          >
-            {truncate(title as string, 12)}
-          </h3>
+          <div className="mt-4 mb-2 flex items-center justify-between">
+            <h3
+              className={`select-none text-xl font-medium sm:text-2xl `}
+              title={title}
+            >
+              {truncate(title as string, isMobile ? 6 : 8)}
+            </h3>
+            <input
+              type="checkbox"
+              onChange={onSelect}
+              checked={selected}
+              className="h-5 w-5 rounded border-gray-300 bg-gray-100 text-blue-600 dark:border-gray-600 dark:bg-gray-700"
+            />
+          </div>
+
           <span className="inline-flex divide-x overflow-hidden rounded-md border bg-white shadow-sm">
             <a
-              className="inline-block p-2 sm:p-3 text-gray-700 hover:bg-gray-50 focus:relative"
+              className="inline-block p-1 text-gray-700 hover:bg-gray-50 focus:relative sm:p-2"
               title="Download"
               download={title}
               href={url}
             >
-              <HiOutlineDocumentDownload className="h-4 w-4 sm:h-6 sm:w-6" />
+              <HiOutlineDocumentDownload className="h-5 w-5 sm:h-6 sm:w-6" />
             </a>
             <FileDeteteModal
               id={id as string}
@@ -94,13 +113,28 @@ const Card: React.FC<CardProps> = ({
               filename={title as string}
             />
             <div
-              className="inline-block p-2 sm:p-3 text-gray-700 hover:bg-gray-50 focus:relative"
+              className="inline-block p-1 text-gray-700 hover:bg-gray-50 focus:relative sm:p-2"
               title="Open"
               onClick={() =>
                 router.push(`/view?url=${encodeURIComponent(url as string)}`)
               }
             >
-              <BiWindowOpen className="h-4 w-4 sm:h-6 sm:w-6" />
+              <BiWindowOpen className="h-5 w-5 sm:h-6 sm:w-6" />
+            </div>
+            <div
+              className="inline-block p-1 text-gray-700 hover:bg-gray-50 focus:relative sm:p-2"
+              title={isStarred ? "Starred" : "Star"}
+              onClick={() =>
+                starred.mutate({
+                  id: id as string,
+                  starred: !isStarred,
+                })
+              }
+            >
+              <MdStar
+                className="h-5 w-5 sm:h-6 sm:w-6"
+                fill={isStarred ? "gold" : ""}
+              />
             </div>
           </span>
         </div>
