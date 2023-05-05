896873f2bbaa6b91f3a336383be89f4732ac0b0e
Ahmad A <ahmad@dataprism.me>
2023-05-05 10:57:30 +0500

    fixed bug on closing the header in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,9 @@ function Navbar() {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
-              onClick={() => setIsExpanded(!isExpanded)}
+              onClick={() => {
+                if (!isExpanded) setIsExpanded(true);
+              }}
               type="button"
               className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
